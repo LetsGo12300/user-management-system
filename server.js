d815c0b96085bc43ab5c0ddfe453c0d86a0af077
kat <katkatvdd@yahoo.com.ph>
2022-06-11 15:45:35 +0800

    Integrated MongoDB database users

--- server.js
+++ server.js
@@ -1,20 +1,44 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 const PORT = 3000;
 
 // middleware - body parser
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(favicon(path.join(__dirname, '/favicon.ico')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public', '/index.html'));
-});
+MongoClient.connect(
+  'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.53gsr.mongodb.net/?retryWrites=true&w=majority',
+  { useUnifiedTopology: true }
+)
+  .then((client) => {
+    console.log('Connected to Database');
+    const db = client.db('user-management-system');
+    const users = db.collection('users');
 
-app.post('/users', (req, res) => {
-  console.log('User successfully created');
-});
+    app.get('/', (req, res) => {
+      db.collection('users').find().toArray()
+        .then((results) => {
+          console.log(results);
+        })
+        .catch(error => console.error(error))
+      res.sendFile(path.join(__dirname, '/public', '/index.html'));
+    });
+
+    app.post('/users', (req, res) => {
+      users.insertOne(req.body)
+        .then(() => {
+          console.log('User successfully created');
+          res.redirect('/');
+        })
+        .catch((error) => console.error(error));
+    });
+  })
+  .catch(console.error);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
