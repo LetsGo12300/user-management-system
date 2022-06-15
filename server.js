b2573caca0e3f155aedd24bfeb41983dee2cbd60
kat <katkatvdd@yahoo.com.ph>
2022-06-15 15:29:11 +0800

    Modified PORT assignment for heroku

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const MongoClient = require('mongodb').MongoClient;
 const { ObjectId } = require('mongodb');
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // set EJS as template engine
 app.set('view engine', 'ejs')
